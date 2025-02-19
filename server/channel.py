48cee846c111147a5cdb82635835d0d92be9e534
jus-schl <justus.schlueter@gmx.de>
2025-02-19 16:08:29 +0100

    updating channel.py

--- server/channel.py
+++ server/channel.py
@@ -24,7 +24,7 @@ CHANNEL_AUTHKEY = '0987654321'
 CHANNEL_NAME = "BeachLeague Chat"
 CHANNEL_TOPIC = "Beach Volleyball"
 CHANNEL_WELCOME_MESSAGE = "Welcome to BeachLeague Chat! Talk about beach volleyball, tournaments, and tips."
-CHANNEL_ENDPOINT = "http://vm147.rz.uni-osnabrueck.de/channel.wsgi" # don't forget to adjust in the bottom of the file
+CHANNEL_ENDPOINT = "http://vm147.rz.uni-osnabrueck.de/u078/channel.wsgi" # don't forget to adjust in the bottom of the file
 CHANNEL_FILE = 'messages.json'
 CHANNEL_TYPE_OF_SERVICE = 'aiweb24:chat'
 
@@ -203,5 +203,5 @@ def answering_messages(message_content):
 # run flask --app channel.py register
 # to register channel with hub
 
-if __name__ == '__main__':
-    app.run(port=80, debug=True)
+# if __name__ == '__main__':
+#     app.run(port=80, debug=True)
